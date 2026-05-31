564e9e0f81cb1e45d3166d48520afae80cfdccf5
CPestka <constantin.pestka@c-pestka.de>
2026-05-31 10:10:06 +0200

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -99,6 +99,26 @@ treesitter.install(
 	}
 )
 
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "*",
+	callback = function(args)
+		local buf = args.buf
+		local ft = vim.bo[buf].filetype
+
+		local lang = vim.treesitter.language.get_lang(ft)
+		if not lang then
+			return
+		end
+
+		local ok_add = pcall(vim.treesitter.language.add, lang)
+		if not ok_add then
+			return
+		end
+
+		pcall(vim.treesitter.start, buf, lang)
+	end,
+})
+
 local telescope = require("telescope")
 local actions = require("telescope.actions")
 telescope.setup(
